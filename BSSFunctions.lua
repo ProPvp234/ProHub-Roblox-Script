03334a31833322f9923812d06ae53b5872e595af
Bulba 001 <87039622+ProPvp234@users.noreply.github.com>
2023-02-01 17:34:40 +0100

    Update and rename safe.lua to BSSFunctions.lua

---
+++ BSSFunctions.lua
@@ -0,0 +1,46 @@
+local bss = {}
+
+function bss:CheckVicius()
+  local Monsters = game.Workspace.Monsters
+	if Monsters:FindFirstChild("Vicious Bee (Lvl 1)") then
+		Notification("ProHub", "Vicious Bee (Lvl 1) Finded", "7733746980", 5)
+		Character.HumanoidRootPart.CFrame = Monsters:FindFirstChild("Vicious Bee (Lvl 1)").HumanoidRootPart.CFrame
+	elseif Monsters:FindFirstChild("Vicious Bee (Lvl 2)") then
+		Notification("ProHub", "Vicious Bee (Lvl 2) Finded", "7733746980", 5)
+		Character.HumanoidRootPart.CFrame = Monsters:FindFirstChild("Vicious Bee (Lvl 2)").HumanoidRootPart.CFrame
+	elseif Monsters:FindFirstChild("Vicious Bee (Lvl 3)") then
+		Notification("ProHub", "Vicious Bee (Lvl 3) Finded", "7733746980", 5)
+		Character.HumanoidRootPart.CFrame = Monsters:FindFirstChild("Vicious Bee (Lvl 3)").HumanoidRootPart.CFrame
+	elseif Monsters:FindFirstChild("Vicious Bee (Lvl 4)") then
+		Notification("ProHub", "Vicious Bee (Lvl 4) Finded", "7733746980", 5)
+		Character.HumanoidRootPart.CFrame = Monsters:FindFirstChild("Vicious Bee (Lvl 4)").HumanoidRootPart.CFrame
+	elseif Monsters:FindFirstChild("Vicious Bee (Lvl 5)") then
+		Notification("ProHub", "Vicious Bee (Lvl 5) Finded", "7733746980", 5)
+		Character.HumanoidRootPart.CFrame = Monsters:FindFirstChild("Vicious Bee (Lvl 5)").HumanoidRootPart.CFrame
+	elseif Monsters:FindFirstChild("Vicious Bee (Lvl 6)") then
+		Notification("ProHub", "Vicious Bee (Lvl 6) Finded", "7733746980", 5)
+		Character.HumanoidRootPart.CFrame = Monsters:FindFirstChild("Vicious Bee (Lvl 6)").HumanoidRootPart.CFrame
+	elseif Monsters:FindFirstChild("Vicious Bee (Lvl 7)") then
+		Notification("ProHub", "Vicious Bee (Lvl 7) Finded", "7733746980", 5)
+		Character.HumanoidRootPart.CFrame = Monsters:FindFirstChild("Vicious Bee (Lvl 7)").HumanoidRootPart.CFrame
+	elseif Monsters:FindFirstChild("Vicious Bee (Lvl 8)") then
+		Notification("ProHub", "Vicious Bee (Lvl 8) Finded", "7733746980", 5)
+		Character.HumanoidRootPart.CFrame = Monsters:FindFirstChild("Vicious Bee (Lvl 8)").HumanoidRootPart.CFrame
+	elseif Monsters:FindFirstChild("Vicious Bee (Lvl 9)") then
+		Notification("ProHub", "Vicious Bee (Lvl 9) Finded", "7733746980", 5)
+		Character.HumanoidRootPart.CFrame = Monsters:FindFirstChild("Vicious Bee (Lvl 9)").HumanoidRootPart.CFrame
+	elseif Monsters:FindFirstChild("Vicious Bee (Lvl 10)") then
+		Notification("ProHub", "Vicious Bee (Lvl 10) Finded", "7733746980", 5)
+		Character.HumanoidRootPart.CFrame = Monsters:FindFirstChild("Vicious Bee (Lvl 10)").HumanoidRootPart.CFrame
+	elseif Monsters:FindFirstChild("Vicious Bee (Lvl 11)") then
+		Notification("ProHub", "Vicious Bee (Lvl 11) Finded", "7733746980", 5)
+		Character.HumanoidRootPart.CFrame = Monsters:FindFirstChild("Vicious Bee (Lvl 11)").HumanoidRootPart.CFrame
+	elseif Monsters:FindFirstChild("Vicious Bee (Lvl 12)") then
+		Notification("ProHub", "Vicious Bee (Lvl 12) Finded", "7733746980", 5)
+		Character.HumanoidRootPart.CFrame = Monsters:FindFirstChild("Vicious Bee (Lvl 12)").HumanoidRootPart.CFrame
+	else
+		Notification("ProHub", "Visius Bee Not Found", "7733745572", 5)
+	end
+end
+
+return bss
--- safe.lua
+++
@@ -1,3 +0,0 @@
-function test()
-  print("jebanie")
-end
